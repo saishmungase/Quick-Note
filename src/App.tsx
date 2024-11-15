13340187110ce1fd09ff3cdfac44ebb1749950b2
Saish Mungase <saishmungase@gmail.com>
2024-11-16 01:02:23 +0530

    AddData-Added-To-App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import AddData from './Components/AddData'
 import Header from './Components/Header'
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <AddData />
     </div>
   )
 }
--- src/Components/AddData.tsx
+++ src/Components/AddData.tsx
@@ -1,8 +1,43 @@
-export default function AddData(){
+import { useState } from "react";
+
+export default function AddData() {
+    const [card, setCard] = useState({
+        title: "",
+        content: ""
+    });
+
+    function handleChange(event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
+        const { name, value } = event.target;
+        setCard((prevVal) => ({
+            ...prevVal,
+            [name]: value 
+        }));
+    }
+
+    function submit() {
+        localStorage.setItem(card.title, card.content);
+        console.log(
+            `Saved: ${localStorage.getItem(card.title)}`
+        );
+    }
 
     return (
         <div className="add-data">
-            
+            <form onSubmit={submit}> 
+                <input
+                    name="title"
+                    placeholder="Title"
+                    onChange={handleChange}
+                    value={card.title}
+                />
+                <textarea
+                    name="content"
+                    placeholder="Content"
+                    onChange={handleChange}
+                    value={card.content}
+                />
+                <button type="submit">+</button>
+            </form>
         </div>
-    )
+    );
 }
