a9e50d2bcdfaa36a841f5ebb719b69f192666946
Saish Mungase <saishmungase@gmail.com>
2024-11-16 12:22:11 +0530

    Note-deleteCard-Added

--- src/Components/Note.tsx
+++ src/Components/Note.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import deleteIcon from '../assets/delete.png';
 
 type propsType = {
@@ -7,12 +8,17 @@ type propsType = {
 
 export default function Note(props : propsType){
 
+    function deleteNote(event: React.MouseEvent<HTMLImageElement>){
+        const deleteKey: string = event.currentTarget.alt
+        localStorage.removeItem(deleteKey)
+        location.reload();
+    }
 
     return <div className="note-box">
         <h2>{props.title}</h2>
         <p>{props.content}</p>
         <div className="delete-box">
-            <img src={deleteIcon}></img>
+            <img onClick={deleteNote} src={deleteIcon} alt={props.title}></img>
         </div>
     </div>
 }
